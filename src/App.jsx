70f6f3a6324d30bab9a998c6e2b7a2ab887a3357
unknown <prachiraghav.pr@gmail.com>
2024-08-23 23:53:24 +0530

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import WeatherForm from './components/WeatherForm';
+import WeatherDetails from './components/WeatherDetails';
+import WeatherForecast from './components/WeatherForecast';
+
+const API_KEY = '76bde0717dfb4dd252ac6ce284e36507'; // API key from openweathermap
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+function App() {
+  const [weather, setWeather] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [forecast, setForecast] = useState(null);
+  const [unit, setUnit] = useState('metric');
+  const [error, setError] = useState('');
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    setWeather(null);
+    setWind(null);
+    setForecast(null);
+    setUnit('metric');
+    setError('');
+    setBackgroundImage(back);
+  };
+
+  const fetchWeather = async (city, unit) => {
+    try {
+      const weatherResponse = await axios.get(`${BASE_URL}/weather`, {
+        params: {
+          q: city,
+          units: unit,
+          appid: API_KEY,
+        },
+      });
+
+      const forecastResponse = await axios.get(`${BASE_URL}/forecast`, {
+        params: {
+          q: city,
+          units: unit,
+          appid: API_KEY,
+        },
+      });
+     // console.log(weatherResponse.data);
+      //console.log(forecastResponse.data);
+
+      const weatherData = weatherResponse.data;
+      const forecastData = forecastResponse.data.list.filter((item, index) => index % 8 === 0) // Pick one forecast per day
+        .map(item => ({
+          date: new Date(item.dt * 1000).toLocaleDateString(),
+          temp: item.main.temp.toFixed(1),
+          description: item.weather[0].main,
+          icon: item.weather[0].icon,
+        }));
+       
+      //setWeather(weatherData.main);
+      setWeather(weatherData);
+      setWind(weatherData.wind);
+      setForecast(forecastData);
+      setError('');
+      } catch (err) {
+      setError('City not found or error fetching data');
+      setWeather(null);
+      setForecast(null);
+    }
+    
+  };
+  
+
+  return (
+    <div className="app">
+      <div className='nav'>
+        <h2>WASSERFORECAST</h2>
+      </div>
+        <WeatherForm onSearch={fetchWeather} onUnitChange= {setUnit}/>     
+        <button className='clear' onClick={handleClear}>X</button> 
+        <WeatherDetails weather={weather} wind={wind}/>
+
+
+      {error && <p className="error">{error}</p>}      
+  
+      <WeatherForecast forecast={forecast} />
+    </div>
+  );
+}
+
+export default App;
